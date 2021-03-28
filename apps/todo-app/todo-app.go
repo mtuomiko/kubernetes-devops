40418702cecdf80f7d49d5e76b08598f37c50f20
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-03-28 19:17:09 +0300

    Part 1 exercise 12

--- apps/todo-app/todo-app.go
+++ apps/todo-app/todo-app.go
@@ -1,22 +1,135 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+type Config struct {
+	SharedDir     string
+	PublicDir     string
+	ImagePath     string
+	ImageDataPath string
+	ImageLink     string
+	FetchURL      string
+}
+
+type Image struct {
+	FetchTime time.Time `json:"fetchTime"`
+}
+
 func main() {
+	sharedDir := filepath.Join(".", "shared")
+	publicDir := filepath.Join(".", "public")
+	config := Config{
+		SharedDir:     sharedDir,
+		PublicDir:     publicDir,
+		ImagePath:     filepath.Join(sharedDir, "image.jpg"),
+		ImageDataPath: filepath.Join(sharedDir, "image.json"),
+		FetchURL:      "https://picsum.photos/400",
+	}
 	// Port fallback
 	port := "5678"
 	if portEnv, ok := os.LookupEnv("PORT"); ok {
 		port = portEnv
 	}
 
-	r := gin.Default()
-	r.Static("/", "./public")
+	fetchTime := readImageFetchTime(&config)
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(CheckImageStatus(&fetchTime, &config))
+	e.File("/image.jpg", config.ImagePath)
+	e.Static("/", config.PublicDir)
 
 	log.Printf("Server started on port %s", port)
-	r.Run(":" + port)
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+// Echo middleware which checks if the image needs to be refreshed
+func CheckImageStatus(fetchTime *time.Time, config *Config) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			now := time.Now()
+			if fetchTime.IsZero() || !equalDate(*fetchTime, now) {
+				saveImage(config)
+				saveImageData(config, now)
+				*fetchTime = now
+			}
+			return next(c)
+		}
+	}
+}
+
+func readImageFetchTime(config *Config) time.Time {
+	_, err := os.Open(config.ImagePath)
+	if err != nil {
+		return time.Time{}
+	}
+
+	imageDataJson, err := os.ReadFile(config.ImageDataPath)
+	if err != nil {
+		return time.Time{}
+	}
+
+	var imageData Image
+	json.Unmarshal(imageDataJson, &imageData)
+
+	return imageData.FetchTime
+}
+
+func equalDate(time1, time2 time.Time) bool {
+	y1, m1, d1 := time1.Date()
+	y2, m2, d2 := time2.Date()
+	return y1 == y2 && m1 == m2 && d1 == d2
+}
+
+func saveImage(config *Config) {
+	res, err := http.Get(config.FetchURL)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if res.StatusCode <= 199 || res.StatusCode >= 300 {
+		log.Println("Image fetch failed. Resulted in status: " + res.Status)
+		return
+	}
+	defer res.Body.Close()
+
+	file, err := os.Create(config.ImagePath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func saveImageData(config *Config, time time.Time) {
+	imageData := Image{
+		FetchTime: time,
+	}
+	imageDataJson, err := json.Marshal(imageData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if err = ioutil.WriteFile(config.ImageDataPath, imageDataJson, os.ModePerm); err != nil {
+		log.Println(err)
+	}
 }
--- part_01/exercise_11.md
+++ part_01/exercise_11.md
@@ -1,6 +1,6 @@
 ## Part 1 exercise 11
 
-Apps located in [/apps/ticker-server](https://github.com/mtuomiko/kubernetes-devops/tree/main/apps/ticker-server) and [/apps/pingpong](https://github.com/mtuomiko/kubernetes-devops/tree/main/apps/pingpong) at commit **placeholder**. Published through Docker Hub at [mtuomiko/ticker-server](https://hub.docker.com/r/mtuomiko/ticker-server) with tag `0.0.2` and [mtuomiko/pingpong](https://hub.docker.com/r/mtuomiko/pingpong) with tag `0.0.2`.
+Apps located in [/apps/ticker-server](https://github.com/mtuomiko/kubernetes-devops/tree/main/apps/ticker-server) and [/apps/pingpong](https://github.com/mtuomiko/kubernetes-devops/tree/main/apps/pingpong) at commit https://github.com/mtuomiko/kubernetes-devops/commit/80513372d791ebca57c2248546c460c49e7d7b85. Published through Docker Hub at [mtuomiko/ticker-server](https://hub.docker.com/r/mtuomiko/ticker-server) with tag `0.0.2` and [mtuomiko/pingpong](https://hub.docker.com/r/mtuomiko/pingpong) with tag `0.0.2`.
 
 1. The persistent volume directory was created on agent-0 node where our apps seem to run, but that's not a safe assumption. We'll go with it for these exercises. Let's create the volume and the claim with `kubectl apply -f manifests/persistentvolume.yaml` and `kubectl apply -f manifests/persistentvolumeclaim.yaml`
 
