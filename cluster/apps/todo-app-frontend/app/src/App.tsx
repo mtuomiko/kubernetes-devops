7b060274fbea4455a157dc8cc9ca8dde74a38a7d
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-07-08 16:32:35 +0300

    Try full build #force_build

--- cluster/apps/todo-app-frontend/app/src/App.tsx
+++ cluster/apps/todo-app-frontend/app/src/App.tsx
@@ -41,7 +41,7 @@ const App = () => {
 
   return (
     <div>
-      <h3>Todo app (GitOps updates, assemble!)</h3>
+      <h3>Todo app (GitOps, GO!)</h3>
       <img src={BACKEND_URL + '/image.jpg'} />
       <form onSubmit={addTodo}>
         <input
