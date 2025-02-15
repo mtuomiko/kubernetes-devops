83c7d13732aae100cada4e176eb3135f746a1890
Matti Tuomikoski <mtuomikoski@gmail.com>
2025-02-15 19:37:20 +0200

    Part 1 exercise 11

--- apps_v2/pingpong/pingpong.go
+++ apps_v2/pingpong/pingpong.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -10,30 +11,69 @@ import (
 	"time"
 )
 
+type CountStruct struct {
+	Count int `json:"count"`
+}
+
 func main() {
 	port := "4000"
 	if portEnv, ok := os.LookupEnv("PORT"); ok {
 		port = portEnv
 	}
+	countFilePath, ok := os.LookupEnv("COUNT_FILE")
+	if !ok {
+		log.Fatal("COUNT_FILE env var not found")
+	}
+	log.Printf("Using count file path: %s", countFilePath)
 
-	counter := 0
+	counter := getOrInitCount(countFilePath)
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		handlePing(w, r, &counter)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handlePing(w, r, &counter, countFilePath)
 	})
 
 	log.Printf("Pingpong server started in port %s", port)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func handlePing(w http.ResponseWriter, r *http.Request, c *int) {
+func handlePing(w http.ResponseWriter, r *http.Request, counter *int, countFilePath string) {
 	logMsg := fmt.Sprintf("%s: %s %s", time.Now().Format(time.RFC3339), r.Method, r.URL.Path)
 	log.Println(logMsg)
 
 	if r.Method == "GET" && r.URL.Path == "/ping" {
-		io.WriteString(w, "pong "+strconv.Itoa(*c)+"\n")
-		*c++
+		io.WriteString(w, "pong "+strconv.Itoa(*counter)+"\n")
+		*counter++
+		saveCount(*counter, countFilePath)
 	} else {
 		http.NotFound(w, r)
+	}
+}
+
+func getOrInitCount(countFilePath string) int {
+	jsonData, err := os.ReadFile(countFilePath)
+
+	if err == nil {
+		var jsonCount CountStruct
+		json.Unmarshal(jsonData, &jsonCount)
+		return jsonCount.Count
+	} else {
+		// assume any errors are because file does not yet exist
+		saveCount(0, countFilePath)
+		return 0
+	}
+}
+
+func saveCount(count int, countFilePath string) {
+	countStruct := CountStruct{
+		Count: count,
+	}
+	countJson, err := json.Marshal(countStruct)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = os.WriteFile(countFilePath, countJson, os.ModePerm)
+	if err != nil {
+		log.Println(err)
 	}
 }
--- apps_v2/ticker-server/ticker-server.go
+++ apps_v2/ticker-server/ticker-server.go
@@ -11,6 +11,16 @@ import (
 type StatusStruct struct {
 	UUID      string `json:"uuid"`
 	Timestamp string `json:"timestamp"`
+	Count     int    `json:"pingPongCount"`
+}
+
+type TickerStruct struct {
+	UUID      string `json:"uuid"`
+	Timestamp string `json:"timestamp"`
+}
+
+type CountStruct struct {
+	Count int `json:"count"`
 }
 
 func main() {
@@ -25,34 +35,64 @@ func main() {
 	}
 	log.Printf("Using status file path: %s", statusFilePath)
 
+	countFilePath, ok := os.LookupEnv("COUNT_FILE")
+	if !ok {
+		log.Fatal("COUNT env var not found")
+	}
+	log.Printf("Using count file path: %s", countFilePath)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handleStatus(w, r, statusFilePath)
+		handleStatus(w, r, statusFilePath, countFilePath)
 	})
 
-	log.Printf("Status server started in port %s", port)
+	log.Printf("Status server starting in port %s", port)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func handleStatus(w http.ResponseWriter, r *http.Request, statusFilePath string) {
+func handleStatus(w http.ResponseWriter, r *http.Request, statusFilePath string, countFilePath string) {
 	log.Printf("%s %s", r.Method, r.URL.Path)
 	if r.Method == "GET" && r.URL.Path == "/status" {
-		statusJson, err := json.Marshal(readStatus(statusFilePath))
+		ticker := readTicker(statusFilePath)
+		count := readCount(countFilePath)
+
+		status := &StatusStruct{
+			UUID:      ticker.UUID,
+			Timestamp: ticker.Timestamp,
+			Count:     count.Count,
+		}
+
+		statusJson, err := json.Marshal(status)
 		if err != nil {
 			fmt.Println(err)
+			http.Error(w, "500 infernal server error", http.StatusInternalServerError)
 		}
+
+		w.Header().Set("Content-Type", "application/json")
 		w.Write(statusJson)
 	} else {
 		http.NotFound(w, r)
 	}
 }
 
-func readStatus(path string) StatusStruct {
+func readTicker(path string) TickerStruct {
 	jsonData, err := os.ReadFile(path)
 	if err != nil {
 		log.Println(err)
 	}
 
-	var status StatusStruct
-	json.Unmarshal(jsonData, &status)
-	return status
+	var ticker TickerStruct
+	json.Unmarshal(jsonData, &ticker)
+	return ticker
+}
+
+func readCount(path string) CountStruct {
+	log.Printf("starting to read count from %s", path)
+	jsonData, err := os.ReadFile(path)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var count CountStruct
+	json.Unmarshal(jsonData, &count)
+	return count
 }
